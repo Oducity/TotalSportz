7b51638e8c6693bccef13d00969ee4123c0e4595
Oducity <henry.odumamwen@gmail.com>
2026-04-07 20:54:22 +0100

    Developed the getApiData() function in the getData.mjs file and exported the same.

---
+++ src/getData.mjs
@@ -0,0 +1,26 @@
+const cultout = "football-players-search?search=m";
+
+export default async function getApiData(para, api) {
+  const url = `https://free-api-live-football-data.p.rapidapi.com/${para}`;
+  const options = {
+    method: "GET",
+    headers: {
+      "x-rapidapi-key": api,
+      "x-rapidapi-host": "free-api-live-football-data.p.rapidapi.com",
+      "Content-Type": "application/json",
+    },
+  };
+
+  try {
+    const response = await fetch(url, options);
+    if (!response.ok) {
+      const text = await response.text();
+      console.log(text);
+    }
+    const data = await response.json();
+      console.log(data);
+      return data;
+  } catch (error) {
+    console.error(error);
+  }
+}
