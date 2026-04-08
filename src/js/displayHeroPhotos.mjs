d4058537fcd5e7807d5b1a334312f073f78479c3
Oducity <henry.odumamwen@gmail.com>
2026-04-08 20:03:31 +0100

    Added class and id attributes to nav>a elements.

--- src/js/displayHeroPhotos.mjs
+++ src/js/displayHeroPhotos.mjs
@@ -1,41 +1,6 @@
-//export default async function displayHeroPhotos(path) {
-//  try {
-//    const response = await fetch(path);
-//    if (!response.ok) {
-//        throw new Error(`Response not ok: ${response.status}`);
-//    } else {
-//        const data = await response.json();
-//        console.log(data);
-//    }
-//  } catch (error) {
-//    console.error(error);
-//  }
-//}
+export default function displayHeroPhotos() {
+  document.querySelector("title").textContent = "TotalSportz || Home - page";
+  const main = document.querySelector("#main");
+  main.innerHTML = `<h1>${document.querySelector("#home")}</h1>`
 
-export default async function getHeroData(repo, path, jsonfile) {
-    
-    try {
-        const testurl =
-          "https://corsproxy.io/?key=a22051f7&url=https://github.com/Oducity/TotalSportz/tree/main/src/public/json/home.json";
-        //const targetUrl = `https://oducity.github.io/${repo}/${path}/${jsonfile}`;
-        //const proxyUrl = `https://corsproxy.io/?key=a22051f7&url=${targetUrl}`;
-        //const options = {
-        //  method: "GET",
-        //  headers: {
-        //    "x-rapidapi-key": "a22051f7",
-        //    "x-rapidapi-host": "https://corsproxy.io",
-        //    "Content-Type": "application/json",
-        //  },
-        //};
-        const response = await fetch(testurl);
-    if (!response.ok) {
-      const text = await response.text();
-      console.log(text);
-    }
-    const data = await response.json();
-      console.log(data);
-      return data;
-  } catch (error) {
-    console.error(error);
-  }
 }
--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,9 @@
 const myAppiKey = "d5e371912emshe979d50bdbd81f0p15c7c5jsn0e0e5bdbf686";
 
 import { lastModify, copyRight } from "./lastmodified.mjs";
-import getHeroData from "./displayHeroPhotos.mjs";
-const api = "";
-const repo = "TotalSportz";
-const path = "src/public/json";
-const jsonfile = "home.json";
+import displayHeroPhotos from "./displayHeroPhotos.mjs";
 
-getHeroData(repo, path, jsonfile);
 
+//splayHeroPhotos();
 lastModify();
 copyRight();
