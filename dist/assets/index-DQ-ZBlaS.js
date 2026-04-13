5b53e72e871cb8f40524305b54e6f235035e361f
Oducity <henry.odumamwen@gmail.com>
2026-04-14 00:27:07 +0100

    Ran npm run build.

--- dist/assets/index-DQ-ZBlaS.js
+++ dist/assets/index-DQ-ZBlaS.js
@@ -1,12 +1,12 @@
 (function(){let e=document.createElement(`link`).relList;if(e&&e.supports&&e.supports(`modulepreload`))return;for(let e of document.querySelectorAll(`link[rel="modulepreload"]`))n(e);new MutationObserver(e=>{for(let t of e)if(t.type===`childList`)for(let e of t.addedNodes)e.tagName===`LINK`&&e.rel===`modulepreload`&&n(e)}).observe(document,{childList:!0,subtree:!0});function t(e){let t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin===`use-credentials`?t.credentials=`include`:e.crossOrigin===`anonymous`?t.credentials=`omit`:t.credentials=`same-origin`,t}function n(e){if(e.ep)return;e.ep=!0;let n=t(e);fetch(e.href,n)}})();async function e(e,t,n,r){let i=`https://${e}${t}`,a={method:`GET`,headers:{"x-rapidapi-key":n,"x-rapidapi-host":e,"Content-Type":r}};try{let e=await fetch(i,a);if(e.ok)return await e.json();{let t=await e.text();console.log(t)}}catch(e){console.error(e)}}function t(){let e=`Last Updated: ${new Date().toLocaleDateString(`en-US`)}`;document.querySelector(`#latestModified`).textContent=e}function n(e){let t=new Date().getFullYear();document.querySelector(`#yearcopyright`).textContent=t.toString()}function r(){document.querySelector(`title`).textContent=`TotalSportz || Home - page`;let e=document.querySelector(`#main`);e.innerHTML=`
-  <div class="hero-container">${document.createElement(`img`).src=`../images/hero1.webp`}</div>
+  <div class="hero-container"><img id="hero-image"></div>
   <h1>${document.querySelector(`#home`).textContent}</h1>
   <p class="intro">TotalSportz is a leading company that provide real update of live events of sports around the world including past events.
   We delight ourselves in providing free quality services to you and prioritize your satisfaction. <span> <strong>Note: This is a student project</strong></span></p>
   <div class="events" id="events">
   <section class="fixtures" id="fixtures"></section>
   </div>
-  `}async function i(){let t=await e(`therundown-therundown-v1.p.rapidapi.com`,`/events/5527455bb80a5e9884153786aeb5f2b2?include=scores`,`d5e371912emshe979d50bdbd81f0p15c7c5jsn0e0e5bdbf686`,`application/json`);console.log(t);let n=``,r=``;t.teams.forEach(e=>{e.is_home?n=e.name:r=e.name});let i=document.querySelector(`#fixtures`);i.innerHTML=``;let a=document.createElement(`h2`);a.innerHTML=`
+  `}async function i(){let t=await e(`therundown-therundown-v1.p.rapidapi.com`,`/events/5527455bb80a5e9884153786aeb5f2b2?include=scores`,`d5e371912emshe979d50bdbd81f0p15c7c5jsn0e0e5bdbf686`,`application/json`),n=``,r=``;t.teams.forEach(e=>{e.is_home?n=e.name:r=e.name});let i=document.querySelector(`#fixtures`);i.innerHTML=``;let a=document.createElement(`h2`);a.innerHTML=`
     ${n}&nbsp; <span class="vesus">VS</span> &nbsp;${r}
     `;let o=document.createElement(`div`);o.innerHTML=`
     <p> <span>League:</span><span>${t.schedule.league_name}</span></p>
@@ -51,4 +51,4 @@
             </tr>
         </table>
     </div>
-    `,i.appendChild(a),i.appendChild(o)}i(),r(),t(),n();
+    `,i.appendChild(a),i.appendChild(o);let s=document.querySelector(`#hero-image`);s.setAttribute(`SRC`,`https://github.com/Oducity/TotalSportz/blob/main/src/images/hero1.webp`),s.setAttribute(`loading`,`lazy`),s.setAttribute(`alt`,`Hero image of a player happily raising a throphy.`),s.setAttribute(`width`,`320`)}i(),r(),t(),n();
