32e052f5ff6a4c8d1713271d8f05a36bb4f27425
Oducity <henry.odumamwen@gmail.com>
2026-04-14 01:04:27 +0100

    imported hero image from the its directory and used it in the displayHeroPhotos.mjs file.

--- src/js/displayHeroPhotos.mjs
+++ src/js/displayHeroPhotos.mjs
@@ -1,9 +1,9 @@
-
+import heroImage from './assets/images/hero1.webp';
 export default function displayHeroPhotos() {
   document.querySelector("title").textContent = "TotalSportz || Home - page";
   const main = document.querySelector("#main");
   main.innerHTML = `
-  <div class="hero-container"><img id="hero-image"></div>
+  <div class="hero-container"><img id="hero-image" src=${heroImage} alt="Photo of a player happily raising a trophy." width="320" loading="lazy"></div>
   <h1>${document.querySelector("#home").textContent}</h1>
   <p class="intro">TotalSportz is a leading company that provide real update of live events of sports around the world including past events.
   We delight ourselves in providing free quality services to you and prioritize your satisfaction. <span> <strong>Note: This is a student project</strong></span></p>
