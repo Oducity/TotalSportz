40ee0c381317b4d270ac601238b06643e49e241b
Oducity <henry.odumamwen@gmail.com>
2026-04-20 22:17:28 +0100

    Styled the confirmation page and removed the home page api.

--- src/js/displaySportPage.mjs
+++ src/js/displaySportPage.mjs
@@ -6,11 +6,15 @@ export default async function displaySportspage() {
   //Emptied the main element and pass into it the following html h1 and section elements to be used by other functions in the program.
   const main = document.querySelector("main");
   main.innerHTML = "";
+
+
+  //This code is for search bar ---- To be added to the project later.
+    //<label for="search">
+    //<input type="text" id="search" name"search" placeholder="type STANDING to get league standing">
+    //<button id="search-btn" class"search-btn" type="button">Display Result</button>
+    //</label>
+
   main.innerHTML = `
-    <label for="search">
-    <input type="text" id="search" name"search">
-    <button id="search-btn" class"search-btn" type="button">Display Result</button>
-    </label>
     <h1 class="sporpage-heading" id="sportpage-heading">Sports</h1>
     <P>
     This page gives everything you need about football events, from list of nations who paticipate in local and international football competitions to leagues and mony more.
@@ -19,7 +23,7 @@ export default async function displaySportspage() {
     </P>
     <div id="sport-cards" class="sport-cards">
     </div>
-    <dialog>
+    <dialog id="mymodal">
         <div id="dialog-box" class="dialog-box"></div>
         <button id="close-dialog" class="close-dialog" type="button"></button>
     <dialog>
